bfcb41c0769c4d8dda4280488a12779c44baf55a
lucidmachine <lucid.machine@gmail.com>
2019-04-07 19:54:25 -0400

    Add some docstrings

--- src/main/scala/com/lucidmachinery/bitclock/BitClock.scala
+++ src/main/scala/com/lucidmachinery/bitclock/BitClock.scala
@@ -1,10 +1,12 @@
 package com.lucidmachinery.bitclock
 
+/** All digits 0-9. */
 object Digit extends Enumeration {
   type Digit = Value
   val Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine = Value
 }
 
+/** All bits 0-1. */
 object Bit extends Enumeration {
   type Bit = Value
   val Zero, One = Value
@@ -18,13 +20,16 @@ object BitClock {
   type BitDigit = Tuple4[Bit, Bit, Bit, Bit]
   type BitTime = Tuple6[BitDigit, BitDigit, BitDigit, BitDigit, BitDigit, BitDigit]
 
+  /** Returns whether the given place value is active in the given digit. */
   def isActive(placeValue: Int, digit: Digit): Boolean =
     (placeValue & digit.id) > 0
 
+  /** Returns a Bit representing the given place value in the given digit. */
   def bit(placeValue: Int, digit: Digit): Bit =
     if (isActive(placeValue, digit)) Bit.One
     else Bit.Zero
 
+  /** Returns a BitDigit representing the given digit. */
   def bitDigit(digit: Digit): BitDigit = (
     bit(8, digit),
     bit(4, digit),
@@ -32,6 +37,7 @@ object BitClock {
     bit(1, digit),
   )
 
+  /** Returns a BitTime representing the given time. */
   def bitTime(time: LocalTime): BitTime = (
     bitDigit(Digit(time.hour.get / 10)),
     bitDigit(Digit(time.hour.get % 10)),
@@ -41,9 +47,11 @@ object BitClock {
     bitDigit(Digit(time.second.get % 10)),
   )
 
+  /** Returns a BitTime representing the current time. */
   def bitTime(): BitTime =
     bitTime(LocalTime.now())
 
+  /** Prints the current BitTime to stdout. */
   def main(args: Array[String]): Unit = {
     println(bitTime())
   }
